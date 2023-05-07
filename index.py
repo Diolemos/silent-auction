9706e45aca4e0facaa48b598c6b80943bf0c5ae6
Dio Lemos <pedro.devsc@outlook.com>
2023-05-07 00:11:02 -0300

    add art.py module create biders dictionary create aucion function

--- index.py
+++ index.py
@@ -2,3 +2,18 @@ import os
 clear = lambda: os.system('clear')
 
 # to clear the console clear()
+
+biders = {}
+
+
+def auction():
+    continue_auction = 'yes'
+    while continue_auction != 'no':
+      current_bider =   input("What your name?")
+      current_bid = input("What is your bid?")
+      biders[current_bider] = current_bid
+      continue_auction = input('is there more people in the room? type "yes" or "no" ' )
+      clear()
+
+
+auction()        
